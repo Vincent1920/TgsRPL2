0d92008930c9e81b4b8d13c998eb1dcf6ef57125
Vincent1920 <vincentluhulima600@gmail.com>
2025-11-20 17:19:11 +0700

    routes

--- resources/js/Router/index.js
+++ resources/js/Router/index.js
@@ -4,7 +4,9 @@ import Register from '../Auth/Register.vue'
 import Kategori from '../Pages/Kategori.vue'
 import home from '../Pages/home.vue'
 import tugas from '../Pages/tugas.vue'
+import CreateTugasPage from '../Public/CreateTugasPage.vue'
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import CreateKategoriPage from '../Public/CreateKategoriPage.vue'
 
 // import home from '../Pages/home.vue'
 const routes = [
@@ -13,6 +15,9 @@ const routes = [
   { path: '/registrasi', component: Register },
   { path: '/kategori', component: Kategori },
   { path: '/tugas', component: tugas },
+  { path: '/Create_Tugas', component: CreateTugasPage },
+  { path: '/Create_kategori', component: CreateKategoriPage },
+
 
 
 ]
